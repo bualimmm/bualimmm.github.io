893704b9549d9ede4dc7e23887bd7e3c6b48efa5
bualimmm <mahdi.buali@kaust.edu.sa>
2024-06-04 19:41:16 +0300

    added rand plugin

--- _plugins/random_posts.rb
+++ _plugins/random_posts.rb
@@ -2,7 +2,7 @@ module Jekyll
     module RandomPostsFilter
       def random_posts(site, locale, limit = 5)
         randoms_collection = site.collections.find { |collection| collection.label == 'randoms' }
-        filtered_docs = randoms_collection.docs.select { |doc| doc.url.start_with?("/#{locale}/random/") }
+        filtered_docs = randoms_collection.docs.select { |doc| doc.data['locale'] == locale }
   
         puts "Filtered documents for locale '#{locale}':"  # Debugging output
         filtered_docs.each { |doc| puts doc.path }
@@ -13,3 +13,39 @@ module Jekyll
   end
   
   Liquid::Template.register_filter(Jekyll::RandomPostsFilter)
+
+
+  module Jekyll
+    module RandomPostsFilter
+      def random_posts(site, locale, limit = 5)
+        site.collections['randoms'].docs
+          .select { |doc| doc.data['locale'] == locale }
+          .sample(limit)
+      end
+    end
+  end
+  
+  Liquid::Template.register_filter(Jekyll::RandomPostsFilter)
+
+  module Jekyll
+    module RandomPostsFilter
+      def random_posts(site, locale, limit = 5)
+        site.collections['randoms'].docs
+          .select { |doc| doc.url.start_with?("/#{locale}/random/") }
+          .sample(limit)
+      end
+    end
+  end
+
+  module Jekyll
+    module RandomPostsFilter
+      def random_posts(site, locale, limit = 5)
+        randoms_collection = site.collections.find { |collection| collection.label == 'randoms' }
+        randoms_collection.docs
+          .select { |doc| doc.data['locale'] == locale }
+          .sample(limit)
+      end
+    end
+  end
+  
+  
