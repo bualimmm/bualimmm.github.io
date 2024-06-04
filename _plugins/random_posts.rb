f07103e4dcd6e228e890462f67bf68c0baaf354e
bualimmm <mahdi.buali@kaust.edu.sa>
2024-06-04 19:42:00 +0300

    added rand plugin

--- _plugins/random_posts.rb
+++ _plugins/random_posts.rb
@@ -13,39 +13,3 @@ module Jekyll
   end
   
   Liquid::Template.register_filter(Jekyll::RandomPostsFilter)
-
-
-  module Jekyll
-    module RandomPostsFilter
-      def random_posts(site, locale, limit = 5)
-        site.collections['randoms'].docs
-          .select { |doc| doc.data['locale'] == locale }
-          .sample(limit)
-      end
-    end
-  end
-  
-  Liquid::Template.register_filter(Jekyll::RandomPostsFilter)
-
-  module Jekyll
-    module RandomPostsFilter
-      def random_posts(site, locale, limit = 5)
-        site.collections['randoms'].docs
-          .select { |doc| doc.url.start_with?("/#{locale}/random/") }
-          .sample(limit)
-      end
-    end
-  end
-
-  module Jekyll
-    module RandomPostsFilter
-      def random_posts(site, locale, limit = 5)
-        randoms_collection = site.collections.find { |collection| collection.label == 'randoms' }
-        randoms_collection.docs
-          .select { |doc| doc.data['locale'] == locale }
-          .sample(limit)
-      end
-    end
-  end
-  
-  
